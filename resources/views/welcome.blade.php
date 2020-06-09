4273e4cef6f14608d829607c1f4126bde83bc681
Kalen Jordan <kalenj@gmail.com>
2020-06-09 15:28:28 -0500

    Populate the .env.example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,8 @@
                 Search your <br class='lg:hidden'/> <span class='text-indigo-600'>tweets</span>
             </h2>
             <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                See a demo of the search functionality which searches Paul Graham's tweets.
+                See a demo of the search functionality which searches
+                <a class="font-bold" href="https://twitter.com/paulg">@paulg</a>'s tweets.
             </p>
             <div class="my-5 max-w-md mx-auto sm:flex sm:justify-center md:my-8">
                 <div class="rounded-md shadow">
@@ -60,23 +61,9 @@
                                 </h5>
                                 <p class="mt-2 text-base leading-6 text-gray-500">
                                     Go into your twitter account and download your data under
-                                    Settings > Account > Your Twitter Data
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="mt-10 md:mt-0">
-                        <div class="flex">
-                            <div class="flex-shrink-0">
-                                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                    @include('svg.icon-eye', ['classes' => 'h-6 w-6'])
-                                </div>
-                            </div>
-                            <div class="ml-4">
-                                <h5 class="text-lg leading-6 font-medium text-gray-900">
-                                    2. Grab the tweet.js file
-                                </h5>
-                                <p class="mt-2 text-base leading-6 text-gray-500">
+                                    Settings > Account > Your Twitter Data.
+
+                                    <br/><br/>
                                     Put the tweet.js file into the storage/app directory. Open it up in nano or vim
                                     and make one small change - remove the beginning "window.YTD.tweet.part0 = " part so that it starts
                                     with the opening bracket [
@@ -93,10 +80,10 @@
                             </div>
                             <div class="ml-4">
                                 <h5 class="text-lg leading-6 font-medium text-gray-900">
-                                    3. Do the first thing!
+                                    2. Setup a free <a class="font-bold" href="https://algolia.com">algolia</a> index
                                 </h5>
                                 <p class="mt-2 text-base leading-6 text-gray-500">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                    Setup a free algolia index - it's free for up to 10k records.
                                 </p>
                             </div>
                         </div>
@@ -110,10 +97,28 @@
                             </div>
                             <div class="ml-4">
                                 <h5 class="text-lg leading-6 font-medium text-gray-900">
-                                    4. Do the fourth thing!
+                                    3. Setup .env file
                                 </h5>
                                 <p class="mt-2 text-base leading-6 text-gray-500">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                    Add your TWITTER_USERNAME along with credentials for a Twitter app - Twitter requires
+                                    you to have an app to use the API even when you're just querying public data.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="mt-10 md:mt-0">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                    @include('svg.icon-eye', ['classes' => 'h-6 w-6'])
+                                </div>
+                            </div>
+                            <div class="ml-4">
+                                <h5 class="text-lg leading-6 font-medium text-gray-900">
+                                    4. Index your tweets
+                                </h5>
+                                <p class="mt-2 text-base leading-6 text-gray-500">
+                                    Run `php artisan algolia:index`
                                 </p>
                             </div>
                         </div>
